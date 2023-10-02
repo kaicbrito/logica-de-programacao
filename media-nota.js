15fb4765e5c911b83eea569eda167e628150c77c
Kaic Brito <kaicbritor@gmail.com>
2023-10-02 14:37:50 -0300

    Create media-nota.js
    :white_check_mark:

---
+++ media-nota.js
@@ -0,0 +1,8 @@
+//Peça ao usuário para inserir três notas e calcule a média delas.
+
+let nota1 = parseFloat(prompt('Digite a primeira nota: '));
+let nota2 = parseFloat(prompt('Digite a segunda nota: '));
+let nota3 = parseFloat(prompt('Digite a terceira nota: '));
+let media = (nota1, nota2, nota3) / 3;
+
+console.log(`A média do aluno é: ${media.toFixed(2)}`);
