6d7f0348daae2272b3776b711eeef3ad4939ae30
Kaic Brito <kaicbritor@gmail.com>
2023-10-02 14:25:06 -0300

    Create somarNumeros.js

---
+++ somarNumeros.js
@@ -0,0 +1,6 @@
+let name = prompt("Digite seu nome: ");
+let n1 = Number(prompt("Digite um numero: "));
+let n2 = Number(prompt("Digite outro numero: "));
+let soma = n1 + n2;
+
+console.log(`Olá ${name}!\nA soma de ${n1} + ${n2} = ${soma}`);
